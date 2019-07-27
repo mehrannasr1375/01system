240bbb9c78cd3d73ea33063017929991317bee36
mehrannasr1375 <mrparadox1397@gmail.com>
2019-07-27 19:45:57 +0430

    add test class for sql injection test

--- test.php
+++ test.php
@@ -1,25 +1,40 @@
 <?php
 require_once ("include_all.php");
+/* a fake class for test sql injection  */
+class Test extends Base
+{
+    public static function testFunction($id)
+    {
+        $con = Base::connect();
+        $query = "SELECT * FROM tbl_user WHERE id='$id';";
+        echo $query."<hr>";
+        $res = $con->query($query);
+        if ($res->rowCount()) {
+            $ret = [];
+            foreach ($res->fetchAll() as $row){
+                $ret[] = $row;
+            }
+        } else
+            $ret = false;
+        Base::disconnect($con);
+        return $ret;
+    }
+}
 
-//$world = trim($_GET['s']);
-//$world = strip_tags($_GET['s']);
-//$world = htmlspecialchars($world);
-
-$posts = Post::searchPosts('ترجمه مقاله', true, true, true, 0, 10);
 
 
 
-echo "<br>";
-echo "<br>";
-echo "<br>";
-var_dump($posts);
-echo "<br>";
-echo "<br>";
-echo "<br>";
-//echo $world;
-echo "<br>";
-echo "<br>";
 
+
+/* sql injection test part */
+if (isset($_GET['id'])) {
+    $result = Test::testFunction($_GET['id']);
+    if (!$result)
+        echo 'user not exists';
+    else
+        var_dump($result);  
+} else
+    echo "no parameters sended! insert id as an query string parameter at the end of url!";
 
 
 
