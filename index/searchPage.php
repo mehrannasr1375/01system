4557bf8d21fb2444ae89a4c21576d22ae9f67dc9
mehrannasr1375 <mrparadox1397@gmail.com>
2019-07-27 13:48:16 +0430

    fix search.php

--- index/searchPage.php
+++ index/searchPage.php
@@ -1,13 +1,12 @@
 <?php
     $world = trim($_GET['s']);
-    $world = strip_tags($_GET['s']);
-    $world = htmlspecialchars($world);
+    $world = htmlspecialchars($world, ENT_QUOTES);
     $last_posts = Post::searchPosts($world, true, true, true, 0, 10);
 
 
     echo '
         <script>
-            $("#navbar").css("background-color"
+            $("#navbar").css("background-color", "black");
         </script>
     ';
 ?>
