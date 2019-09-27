c0763a22f6ba67febe7b741f0292b2df1fe171bd
mehrannasr1375 <mrparadox1397@gmail.com>
2019-09-27 22:58:40 +0330

    add config-sample

---
+++ includes/other/config-sample.php
@@ -0,0 +1,45 @@
+<?php
+
+//PUBLIC CONFIGURE
+define("DEVELOPING_MODE",true);
+define("MAX_POST",5);
+define("MAX_POSTS_TABLE",7);
+define("MAX_PICS_GRID",10);
+define("MAX_LAST_POST",10);
+
+//DATABASE CONFIGURE
+define("HOST_NAME","localhost");
+define("DB_NAME","systemir_my_db");
+define("DB_USER","");
+define("DB_PASS","");
+
+//COOKIES LIFE TIME CONFIGURE
+define("REMEMBER_TIME",60000);
+
+//SMTP CONFIGURE
+define("EMAIL_USER_NAME","");
+define("EMAIL_PASSWORD","");
+
+//SOCIAL NETWORK LINKS CONFIGURE
+define("FACEBOOK_ADDRESS","#");
+define("TELEGRAM_ADDRESS","#");
+define("GOOGLE_PLUS_ADDRESS","#");
+define("INSTAGRAM","#");
+
+//IMAGE RESIZE QUALITIES
+define("POST_IMG_DEFAULT","post_default.jpg");
+define("POST_IMG_QAL_1024X500",70);
+define("POST_IMG_QAL_260X260",70);
+define("MEDIA_IMG_QAL_16X9",50);
+define("MEDIA_IMG_QAL_4X3",50);
+define("MEDIA_IMG_QAL_2X2",30);
+
+
+
+
+
+
+
+
+
+
